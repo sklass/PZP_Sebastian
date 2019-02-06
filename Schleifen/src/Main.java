0ede28037968191719db72d5c24b6f11a3351420
sklass <47379975+sklass@users.noreply.github.com>
2019-02-06 14:33:50 +0100

    Ausbau Rechteck zeichnen

--- Schleifen/src/Main.java
+++ Schleifen/src/Main.java
@@ -1,48 +1,101 @@
 import java.util.Scanner;
 
-            public class Main {
+public class Main {
 
-                public static void main(String[] args) {
-                    Scanner Eingabe = new Scanner(System.in);
-                    String filler = " ";
-                    System.out.println("Bitte länge des Rechtecks eingeben\n");
-                    int laenge = Eingabe.nextInt();
+    public static void main(String[] args) {
+        boolean keep_running = true;
 
-                    System.out.println("Bitte breite des Rechtecks eingeben\n");
-                    int breite = Eingabe.nextInt();
+        while(keep_running == true) {
 
-                    System.out.println("Rechteck ausgefüllt = 1 - Rechteck nicht ausgeüllt = 0\n");
-                    int fill = Eingabe.nextInt();
+            Scanner Eingabe = new Scanner(System.in);
+            char filler = ' ';
+            int dicke = 1;
 
-                    System.out.println("Erzeuge Rechteck mit Breite" + breite + " und Länge " + laenge);
+            //Länge und breite abfragen, in beiden fällen muss der wert mind. 2 betragen
+            System.out.println("Bitte Gesamtlänge des Rechtecks eingeben");
+            int laenge = Eingabe.nextInt();
+            while(laenge < 2) {
+                System.out.println("Fehler! Die Länge muss mindestens 2 betragen\n");
+                laenge = Eingabe.nextInt();
+            }
 
-                    if (fill == 1){
-                        //Zeichen zum füllen des Rechtecks abfragen
-                        System.out.println("Welches Zeichen soll zur Füllung genutzt werden\n");
-                        filler = Eingabe.next();
+            System.out.println("Bitte Gesamtbreite des Rechtecks eingeben");
+            int breite = Eingabe.nextInt();
+            while(breite < 2) {
+                System.out.println("Die Breite muss mindestens 2 betragen\n");
+                breite = Eingabe.nextInt();
+            }
 
-                    }else if(fill == 0) {
-                        System.out.println("Das Rechteck wird nicht gefüllt\n");
-                        filler = " ";
+            //Abfrage ob Rechteck gefüllt werden soll
+            System.out.println("Rechteck ausgefüllt = 1 - Rechteck nicht ausgeüllt = 0");
+            int fill = Eingabe.nextInt();
+
+            //Rechteck füllen Nur möglich wenn mehr als 2 lang und breit
+            if (fill == 1 && laenge > 2 && breite > 2) {
+                //Zeichen zum füllen des Rechtecks abfragen
+                System.out.println("Welches Zeichen soll zur Füllung genutzt werden");
+                filler = Eingabe.next().charAt(0);
+
+            } else {
+                System.out.println("Das Rechteck wird nicht gefüllt\n");
+            }
+
+            System.out.println("Bitte Rahmendicke des Rechtecks eingeben");
+            dicke = Eingabe.nextInt();
+
+            if(dicke*2 > laenge || dicke*2 > breite ){
+                System.out.println("Die eingebenene Rahmen-Dicke kann bei der aktuellen Rechteck größe nicht umgesetzt werden\n Setze Rahmendicke auf 1");
+                dicke = 1;
+            }
+
+            System.out.println("Erzeuge Rechteck mit Breite" + breite + " und Länge " + laenge);
+
+
+            //oberen Rand ausgeben (basierend auf breite + ggf dicke)
+            for(int d = 0 ; d < dicke; d++) {
+                for (int b = 0; b < breite; b++) {
+                    System.out.print("#");
+                    if (b == breite - 1) {
+                        System.out.print("\n");
                     }
-                        //erste Zeile ausgeben (basierend auf breite)
-                        for (int b = 0; b < breite; b++){
-                            System.out.print("#");
-                            if(b == breite-1){
-                                System.out.print("\n");
-                            }
-                        }
-                        //Alle zeilen zwischen erster und letzter
-                        for(int l = 0; l < laenge-2; l++){
-                            System.out.print("#");
-                            for (int bb = 0; bb < breite-2; bb++){
-                                System.out.print(filler);
-                            }
-                            System.out.print("#\n");
-                        }
-                        //letzte Zeile ausgeben
-                        for (int b = 0; b < breite; b++){
-                            System.out.print("#");
-                        }
+                }
+            }
+            //Alle zeilen zwischen erster und letzter
+            for (int l = 0; l < laenge - dicke*2; l++) {
+                //Rahmen ausgeben
+                //links
+                for(int dl = 0; dl < dicke; dl++) {
+                    System.out.print("#");
+                }
+                for (int bb = 0; bb < breite - dicke*2; bb++) {
+                    //Füllung ausgeben
+                    System.out.print(filler);
+                }
+                //Rechts
+                for(int dr= 0; dr < dicke; dr++) {
+                    System.out.print("#");
+                }
+                System.out.print("\n");
+            }
+            //letzte Zeile ausgeben
+            for(int d = 0 ; d < dicke; d++) {
+                for (int b = 0; b < breite; b++) {
+                    System.out.print("#");
+                    if (b == breite - 1) {
+                        System.out.print("\n");
+                    }
+                }
+            }
+
+
+            System.out.println("\nMöchtest du ein weiteres Rechteck zeichnen? (1 = Ja / 0 = Nein)\n");
+            int again = Eingabe.nextInt();
+            if(again == 1){
+                System.out.println("Und gleich nochmal ...");
+            }else{
+                keep_running = false;
+                System.out.println("Bis zum nächsten mal");
+            }
+        }
     }
 }
