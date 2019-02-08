bbeb266bd2fb1a0e4d84e5ffcd96d50d18713e30
sklass <47379975+sklass@users.noreply.github.com>
2019-02-08 18:06:42 +0100

    GeometrieausZahlen hinzugefügt

--- GeometrieAusZahlen/src/Main.java
+++ GeometrieAusZahlen/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     //Deklaration
     static Scanner Eingabe = new Scanner(System.in);
     static int form;
+    static int hoehe;
+    static int breite;
+    static int inhalt = 1;
+    static int kantenlaenge;
 
     public static void main(String[] args) {
 
@@ -13,13 +17,64 @@ public class Main {
         if(form == 1){
             zeichneRechteck();
         }else if(form == 2){
-            zeichneDreieck();
+            System.out.println("Ausrichtung - L/M/R");
+            zeichneDreieck_links();
         }
     }
     static void zeichneRechteck(){
+        System.out.println("Breite Rechteck?");
+        breite = Eingabe.nextInt();
+        System.out.println("Höhe Rechteck?");
+        hoehe = Eingabe.nextInt();
 
+        for(int h = 0; h < hoehe; h++){
+            //Breite
+            for(int b=0; b < breite; b++){
+                System.out.print(inhalt);
+            }
+            System.out.println();
+            inhalt++;
+            if(inhalt == 10){
+                inhalt = 0;
+            }
+        }
     }
-    static void zeichneDreieck(){
+    static void zeichneDreieck_links(){
+        System.out.println("Kantenlänge?");
+        kantenlaenge = Eingabe.nextInt();
 
+        int reihenlaenge = 1;
+        for(int d = 0; d < kantenlaenge; d++){
+            for(int i = 0; i < reihenlaenge; i++ ) {
+                System.out.print(inhalt);
+            }
+            System.out.println();
+            reihenlaenge++;
+            inhalt++;
+            if(inhalt > 9){
+                inhalt = 0;
+            }
+
+        }
     }
+
+    static void zeichneDreieck_rechts(){
+        System.out.println("Kantenlänge?");
+        kantenlaenge = Eingabe.nextInt();
+
+        int reihenlaenge = 1;
+        for(int d = 0; d < kantenlaenge; d++){
+            for(int i = 0; i < reihenlaenge; i++ ) {
+                System.out.print(inhalt);
+            }
+            System.out.println();
+            reihenlaenge++;
+            inhalt++;
+            if(inhalt > 9){
+                inhalt = 0;
+            }
+
+        }
+    }
+
 }
